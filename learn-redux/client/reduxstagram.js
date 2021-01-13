6ebcabf7a527edb5258e4d8ec651966f087106c7
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-01-13 17:57:52 -0500

    Create reduxstagram.js that contains our react-dom render method

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from "react";
+import { render } from "react-dom";
+
+import Main from "./components/Main";
+
+// Import css - webpack handling css loading so we don't need link to stylesheet in index.html
+// Webpack will embed a style tag in the js
+import css from "./styles/style.styl";
+
+render(<Main />, document.getElementById("root"));
