a8443f956b225caaa9bc647e8be286b74b1277c8
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-01-18 18:12:53 -0500

    Update posts.js

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -5,7 +5,15 @@
 
 //we need a reducer for each piece of state
 // this is the posts reducer
+
+// Every time an action is dispatched, EVERY
+// reducer is run (in our case posts and comments)
+// it is up to the code in each reducer to dictate whether
+// that particular reducer actually acts and changes
+// a piece of state
+
 function posts(state = [], action) {
+  console.log("The post will change");
   console.log(state, action);
   return state;
 }
