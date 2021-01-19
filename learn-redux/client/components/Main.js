3ac95c3b837d534a74b576d476774774549f25a8
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-01-18 20:00:10 -0500

    Update Main.js

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -8,6 +8,7 @@ const Main = React.createClass({
         <h1>
           <Link to="/">Reduxstagram</Link>
         </h1>
+        {/* passes down props to first child */}
         {React.cloneElement(this.props.children, this.props)}
       </div>
     );
